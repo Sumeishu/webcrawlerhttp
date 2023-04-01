9c6268a0142be12dbe31911a7ce476cc543c41d0
Sumeishu <1439234574@qq.com>
2023-04-01 13:34:47 +0800

    normalizeURL and tests

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("hello word")
+const urlObj = new URL("https://blog.boot.dev/path/")
+console.log(urlObj.hostname)
+console.log(urlObj.pathname)
+hostpath =  `${urlObj.host}${urlObj.pathname}`
+console.log(hostpath)
+console.log(hostpath.slice(0,-1))
+console.log(hostpath.slice(0,-2))
